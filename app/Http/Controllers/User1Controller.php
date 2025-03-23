e3f412af37fe0c5d3b8d49a4dc5360b5890d6640
kanafreesia <brillantes.shandie@gmail.com>
2025-03-23 20:25:42 +0800

    Added User Controller

---
+++ app/Http/Controllers/User1Controller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+use DB;
+use App\Services\User1Service;
+
+class UserController extends Controller
+{
+    use ApiResponser;
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getUsers()
+    {
+        
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function addUser(Request $request)
+    {
+        
+    }
+
+    public function show($id)
+    {
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    public function delete($id)
+    {
+        
+    }
+
+}
+  
