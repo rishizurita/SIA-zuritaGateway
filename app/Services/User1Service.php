8502bce8d50b46e37eee0ddb6f3a60d09a2daf97
kanafreesia <brillantes.shandie@gmail.com>
2025-03-28 13:39:02 +0800

    done mag gamit access tokens if mag access sa gateway to site 1&2

--- app/Services/User1Service.php
+++ app/Services/User1Service.php
@@ -10,9 +10,12 @@ class User1Service
 
     public $baseUri;
 
+    public $secret;
+
     public function __construct()
     {
         $this->baseUri = config('services.users1.base_uri');
+        $this->secret = config('services.users1.secret');
     }
     
     /**
--- app/Traits/ConsumesExternalService.php
+++ app/Traits/ConsumesExternalService.php
@@ -12,6 +12,11 @@ trait ConsumesExternalService
             'base_uri' => $this->baseUri,
         ]);
 
+
+        if (isset($this->secret)) {
+            $headers['Authorization'] = $this->secret;
+        }
+        
         $response = $client->request($method, $requestUrl, ['form_params' => $form_params, 'headers' => $headers]);
 
         return $response->getBody()->getContents();
