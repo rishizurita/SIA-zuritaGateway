98b6dd8a91d925cee087a34b92b7d32ad422ff95
kanafreesia <brillantes.shandie@gmail.com>
2025-04-10 01:31:36 +0800

    reviving laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,6 +79,7 @@ $app->routeMiddleware([
    //$app->register(App\Providers\AppServiceProvider::class);
 
    $app->register(App\Providers\AuthServiceProvider::class);
+   $app->register(Laravel\Passport\PassportServiceProvider::class);
    $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
    //$app->register(App\Providers\EventServiceProvider::class);
