b8c5b78fcc790b6d7e294c8db783387d68d47fbd
rishizurita <zuritarishi@gmail.com>
2025-04-12 19:24:52 +0800

    handelr

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,21 +2,21 @@
 
 namespace App\Exceptions;
 
-use App\Traits\ApiResponser;
+use App\Traits\ApiResponser; 
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    use ApiResponser;
+    use ApiResponser; 
 
     /**
      * A list of the exception types that should not be reported.
@@ -56,45 +56,52 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        // HTTP not found
         if ($exception instanceof HttpException) {
             $code = $exception->getStatusCode();
-            $message = Response::$statusTexts[$code];
+            // Use SymfonyResponse for status texts.
+            $message = SymfonyResponse::$statusTexts[$code];
 
             return $this->errorResponse($message, $code);
         }
-
+        // Instance not found
         if ($exception instanceof ModelNotFoundException) {
-            $model =    strtolower(class_basename ($exception->getModel()));
-
+            $model = strtolower(class_basename($exception->getModel()));
+            
             return $this->errorResponse("Does not exist any instance of {$model} with the given id", Response::HTTP_NOT_FOUND);
         }
 
+        // Validation exception
         if ($exception instanceof ValidationException) {
-            $errors =  $exception->validator->errors()->getMessages();
-
+            $errors = $exception->validator->errors()->getMessages();
+            
             return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
         }
-
+        // Access forbidden
         if ($exception instanceof AuthorizationException) {
             return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
         }
+        // Unauthorized access
         if ($exception instanceof AuthenticationException) {
-            // Determine the site number
-            $siteNumber = $request->is('users1*') ? 1 : ($request->is('users2*') ? 2 : null);
-
-        
-            return $this->errorResponse([
-                'error' => "Unauthorized",
-                'code' => Response::HTTP_UNAUTHORIZED,
-                'site' => $siteNumber
-            ], Response::HTTP_UNAUTHORIZED);
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
         }
-        
 
+        if ($exception instanceof ClientException) {
+           $message = $exception->getResponse()->getBody();
+           $code = $exception->getCode();
+
+           return $this->errorResponse($message, 200);
+        }
+
+        // If running in development mode, show the original error
         if (env('APP_DEBUG', false)) {
             return parent::render($request, $exception);
         }
 
-        return $this->errorResponse('Unexpected error. Try later', Response::HTTP_INTERNAL_SERVER_ERROR);
+        // Generic error for production
+        return $this->errorResponse(
+            'Unexpected error. Try again later.',
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        );
     }
 }
